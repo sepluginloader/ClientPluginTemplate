ed74c3597d1b10ee490380bb2021da5206fa293f
avaness <austinvaness@gmail.com>
2023-05-06 20:47:39 -0500

    Simplify project

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.IO;
-using ClientPlugin.GUI;
+using System.Reflection;
 using HarmonyLib;
 using Sandbox.Graphics.GUI;
-using ClientPlugin.Config;
-using ClientPlugin.Logging;
-using ClientPlugin.Patches;
-using VRage.FileSystem;
 using VRage.Plugins;
 
 namespace ClientPlugin
@@ -17,106 +12,34 @@ namespace ClientPlugin
         public const string Name = "PluginTemplate";
         public static Plugin Instance { get; private set; }
 
-        public long Tick { get; private set; }
-
-        public IPluginLogger Log => Logger;
-        private static readonly IPluginLogger Logger = new PluginLogger(Name);
-
-        public IPluginConfig Config => config?.Data;
-        private PersistentConfig<PluginConfig> config;
-        private static readonly string ConfigFileName = $"{Name}.cfg";
-
-        private static bool initialized;
-        private static bool failed;
-
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
         public void Init(object gameInstance)
         {
             Instance = this;
 
-            Log.Info("Loading");
-
-            var configPath = Path.Combine(MyFileSystem.UserDataPath, ConfigFileName);
-            config = PersistentConfig<PluginConfig>.Load(Log, configPath);
-
-            if (!PatchHelpers.HarmonyPatchAll(Log, new Harmony(Name)))
-            {
-                failed = true;
-                return;
-            }
-
-            Log.Debug("Successfully loaded");
+            // TODO: Put your one time initialization code here.
+            Harmony harmony = new Harmony(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         public void Dispose()
         {
-            try
-            {
-                // TODO: Save state and close resources here, called when the game exists (not guaranteed!)
-                // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
-            }
-            catch (Exception ex)
-            {
-                Log.Critical(ex, "Dispose failed");
-            }
+            // TODO: Save state and close resources here, called when the game exits (not guaranteed!)
+            // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
 
             Instance = null;
         }
 
         public void Update()
         {
-            EnsureInitialized();
-            try
-            {
-                if (!failed)
-                {
-                    CustomUpdate();
-                    Tick++;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Critical(ex, "Update failed");
-                failed = true;
-            }
-        }
-
-        private void EnsureInitialized()
-        {
-            if (initialized || failed)
-                return;
-
-            Log.Info("Initializing");
-            try
-            {
-                Initialize();
-            }
-            catch (Exception ex)
-            {
-                Log.Critical(ex, "Failed to initialize plugin");
-                failed = true;
-                return;
-            }
-
-            Log.Debug("Successfully initialized");
-            initialized = true;
-        }
-
-        private void Initialize()
-        {
-            // TODO: Put your one time initialization code here. It is executed on first update, not on loading the plugin.
-        }
-
-        private void CustomUpdate()
-        {
             // TODO: Put your update code here. It is called on every simulation frame!
         }
 
-
+        // TODO: Uncomment and use this method to create a plugin configuration dialog
         // ReSharper disable once UnusedMember.Global
-        public void OpenConfigDialog()
+        /*public void OpenConfigDialog()
         {
             MyGuiSandbox.AddScreen(new MyPluginConfigDialog());
-        }
+        }*/
     }
 }
