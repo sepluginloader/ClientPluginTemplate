6ea593a8c061052ce5290723f6874496c801960b
Viktor Ferenczi <viktor@ferenczi.eu>
2024-04-05 18:10:15 +0200

    Deploying the DLL for local testing

--- ClientPlugin/Kill-Space-Engineers.bat
+++
@@ -1,3 +0,0 @@
-REM Pin this to your Start Menu for quicker development!
-@taskkill /f /im SpaceEngineers.exe
-@taskkill /f /im SpaceEngineersLauncher.exe
---
+++ ClientPlugin/deploy.bat
@@ -0,0 +1,28 @@
+@echo off
+if [%2] == [] goto EOF
+
+echo Parameters: %*
+
+set SRC=%~p1
+set NAME=%~2
+
+set TARGET=..\..\..\Bin64\Plugins\Local
+mkdir %TARGET% >NUL 2>&1
+
+echo.
+echo Deploying CLIENT plugin binary:
+echo.
+:RETRY
+ping -n 2 127.0.0.1 >NUL 2>&1
+echo From %1 to "%TARGET%\"
+copy /y %1 "%TARGET%\"
+
+rem TODO: If your plugin depends on any unsafe C# code, then uncomment the next line:
+rem copy /y "%SRC%\System.Runtime.CompilerServices.Unsafe.dll" "%TARGET%\"
+
+IF %ERRORLEVEL% NEQ 0 GOTO :RETRY
+echo Done
+echo.
+exit 0
+
+:EOF
